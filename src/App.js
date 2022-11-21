ce4a2ce96f9869ff9890f2346adf3a1ddbff6cba
David Suy <DavidSuyDev@gmail.com>
2022-11-21 15:20:59 -0800

    add redux and basic menu component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import Footer from './components/Footer/Footer.js'
-import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.js';
+import Header from './components/Header/Header.jsx';
+import { Provider, useDispatch } from 'react-redux';
+import store from './store';
+import Menu from './components/Menu/Menu.jsx';
+
 function App() {
+  console.log(store);
   return (
-    <>
+    <Provider store={store}>
       <Header />
-     <Footer />
-    </>
+      <Menu />
+      <Footer />
+    </Provider>
   );
 }
-  
+
 export default App;
---
+++ src/data.js
@@ -0,0 +1,16 @@
+const menu = [
+  {
+    name: 'dancing shrimp',
+    price: 5,
+    picture: 'url',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio consequuntur ipsa quas magnam totam sequi ducimus facere nesciunt. Obcaecati repellat fuga sed quia, inventore earum a enim in odit accusantium.',
+  },
+  {
+    name: 'dieing shrimp',
+    price: 5,
+    picture: 'url',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio consequuntur ipsa quas magnam totam sequi ducimus facere nesciunt. Obcaecati repellat fuga sed quia, inventore earum a enim in odit accusantium.',
+  },
+];
